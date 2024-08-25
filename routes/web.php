28f57aa5e66e9909edf5e25d7e536419842c0b02
tatun55 <tatun55@gmail.com>
2024-08-25 17:55:56 +0900

    RESTfulなURLにする

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// このアプリケーションはTODO機能しかないので、リダイレクトしてTODO一覧画面に遷移する
 Route::get('/', function () {
-    return view('todo');
+    return redirect()->route('todos.index');
 });
+
+// RESTfulなURLを設定する
+Route::get('todos', function () {
+    return view('todos.index');
+})->name('todos.index');
