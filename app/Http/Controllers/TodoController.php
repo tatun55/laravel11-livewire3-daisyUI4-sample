6f2711fd56605e1f8a71c52c90c7f54f0bf41d47
tatun55 <tatun55@gmail.com>
2024-08-27 15:17:30 +0900

    TODO新規追加処理の保存までを実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -18,6 +19,10 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+        $todo = new Todo();
+        $todo->title = $request->title;
+        $todo->save();
+
+        return redirect()->route('todos.index');
     }
 }
--- database/migrations/2024_08_25_094108_create_todos_table.php
+++ database/migrations/2024_08_25_094108_create_todos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title', 32);
             $table->timestamps();
         });
     }
