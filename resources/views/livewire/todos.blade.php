8e98597f53ca50e97ab6feee596ad82caeae145d
tatun55 <tatun55@gmail.com>
2024-08-31 13:18:04 +0900

    Enterでの追加に対応する

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,9 +1,9 @@
 <div class="h-[100vh] w-full flex flex-col justify-center items-center gap-8">
     <h2 class="text-2xl">TODOリスト</h2>
     {{-- 入力部 --}}
-    <form class="flex w-96">
+    <form wire:submit="add" class="flex w-96">
         <input wire:model="todo" type="text" class="input input-bordered w-full max-w-xs">
-        <button wire:click="add" type="button" class="btn btn-neutral">追加</button>
+        <button type="submit" class="btn btn-neutral">追加</button>
     </form>
     {{-- リスト表示部 --}}
     <ul class="flex flex-col text-left gap-2">
