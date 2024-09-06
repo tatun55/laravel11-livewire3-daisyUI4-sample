7070c17d25b607ea91c382c1acafbfd2a325e50b
tatun55 <tatun55@gmail.com>
2024-09-06 23:51:21 +0900

    モーダルの修正・フォームの追加

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -7,15 +7,18 @@
         {{-- モーダル --}}
         <dialog id="create_modal" class="modal">
             <div class="modal-box">
-                <form method="dialog">
-                    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                </form>
-                <h3 class="text-lg font-bold">タイトル</h3>
-                <p class="py-4">コンテント</p>
+                <button onclick="create_modal.close()" class="btn btn-sm btn-circle btn-ghost absolute right-4 top-4">✕</button>
+                <h3 class="text-lg font-bold">メッセージの入力</h3>
+                <p class="py-4">
+                    <textarea name="message" placeholder="メッセージを入力してください" class="textarea textarea-bordered textarea-md w-full max-w-lg"></textarea>
+                </p>
+                <div class="flex justify-between">
+                    <button onclick="create_modal.close()" class="btn btn-ghost">キャンセル</button>
+                    <button class="btn btn-primary">送信</button>
+                </div>
             </div>
-            <form method="dialog" class="modal-backdrop">
-                <button>close</button>
-            </form>
+            <div onclick="create_modal.close()" class="modal-backdrop"></div>
         </dialog>
+
     </div>
 </div>
