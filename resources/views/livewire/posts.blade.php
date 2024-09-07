0a4a0b407542e459544c34fa81458a26f445edf2
tatun55 <tatun55@gmail.com>
2024-09-07 11:15:50 +0900

    削除モーダルの修正

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -39,7 +39,7 @@
                     </thead>
                     <tbody>
                         @foreach ($posts as $post)
-                            <tr class="hover">
+                            <tr wire:key="post-{{ $post->id }}" class="hover">
                                 <td>{{ $post->message }}</td>
                                 <td>{{ $post->created_at->diffForHumans() }}</td>
                                 <td>{{ $post->updated_at->diffForHumans() }}</td>
@@ -54,7 +54,7 @@
                                         </div>
                                         <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-28 p-2 shadow text-center">
                                             <li>
-                                                <a x-on:click=" openEditModal = true;" class="text-success">
+                                                <a x-on:click="$wire.editPost('{{ $post->id }}'); openEditModal = true;" class="text-success">
                                                     <svg class="w-4" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"></path>
                                                     </svg>
@@ -124,7 +124,7 @@
 
                     <!-- Form -->
                     <form wire:submit='deletePost'>
-                        <p class="p-4 w-full max-w-lg">{{ $post->message }}</p>
+                        <p wire:poll='current_message' class="p-4 w-full max-w-lg">{{ $current_message }}</p>
 
                         <!-- Buttons -->
                         <div class="flex justify-between">
